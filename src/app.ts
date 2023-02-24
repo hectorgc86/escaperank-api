938f27088d8aef19473e0454f41f555f38cf377f
Héctor G C <hectorgc86@gmail.com>
2023-02-24 11:34:52 +0100

    Conexión con base de datos local

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,17 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { router } from "./routes";
+import { dbConnectMySql } from "./config/db";
 
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 //anyadir rutas de origen en cors
 app.use(cors());
 app.use(router);
+
+dbConnectMySql();
+
 app.listen(PORT, () => console.log(`Servicio iniciado en puerto ${PORT}`));
+
---
+++ src/config/db.ts
@@ -0,0 +1,25 @@
+import { Dialect, Sequelize } from "sequelize";
+
+const database = process.env.MYSQL_DATABASE as string;
+const username = process.env.MYSQL_USER as string;
+const password = process.env.MYSQL_PASSWORD;
+const driver = process.env.MYSQL_DRIVER as Dialect;
+const host = process.env.MYSQL_HOST;
+
+const sequelize = new Sequelize(database, username, password,
+  {
+    host,
+    dialect: driver
+  });
+
+const dbConnectMySql = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Conexión correcta con base de datos");
+  }
+  catch (e) {
+    console.log("Error de Conexión con base de datos", e);
+  }
+}
+
+export { sequelize, dbConnectMySql };
