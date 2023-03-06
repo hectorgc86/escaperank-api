446c7668c0fee958b42bb11d28173bf50e905a6c
Héctor G C <hectorgc86@gmail.com>
2023-03-06 19:39:44 +0100

    Agregado prefijo base path v2

--- src/app.ts
+++ src/app.ts
@@ -5,15 +5,15 @@ import { router } from "./routes";
 import { dbConnectMySql } from "./config/db";
 
 const PORT = process.env.PORT || 3001;
+const BASE_PATH = process.env.BASE_PATH as string;
 
 const app = express();
 
 //anyadir rutas de origen en cors
 app.use(cors());
 app.use(express.json());
-app.use(router);
+app.use(BASE_PATH, router);
 
 dbConnectMySql();
 
 app.listen(PORT, () => console.log(`Servicio iniciado en puerto ${PORT}`));
-
